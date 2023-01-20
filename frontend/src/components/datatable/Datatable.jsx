c11940932d9bddb99995b6372194c8d1604d10a9
Clara Zhang <clarazh@seas.upenn.edu>
2023-01-19 20:49:56 -0500

    concat

--- frontend/src/components/datatable/Datatable.jsx
+++ frontend/src/components/datatable/Datatable.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 
 const Datatable = () => {
-  const [data, setData] = useState(userRows);
+  const [data, setData] = useState(userRows[0].concat(userRows[1]));
 
   const handleDelete = (id) => {
     setData(data.filter((item) => item.id !== id));
