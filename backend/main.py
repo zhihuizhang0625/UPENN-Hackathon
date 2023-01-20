56532a89038a1d2048668c6dbadc4a8919dc7b7f
jocelyn <lluo@brynmawr.edu>
2023-01-19 21:41:45 -0500

    submission his

--- backend/main.py
+++ backend/main.py
@@ -130,6 +130,10 @@ def create_table2(AIRTABLE_TABLE_NAME = ''):
     "fields": [
       {
         "description": "Submission history",
+        "name": "title",
+        "type": "singleLineText",
+      },
+      {
         "name": "last_submission_status",
         "type": "singleLineText",
       },
@@ -176,7 +180,7 @@ def create_table2(AIRTABLE_TABLE_NAME = ''):
     print(r)
     
 # import records
-def add_to_airtable2(sub1_status = None, sub1_time = None, sub2_status = None, sub2_time = None,sub3_status = None, sub3_time = None,sub4_status = None, sub4_time = None,sub5_status = None, sub5_time = None, AIRTABLE_TABLE_NAME = ''):
+def add_to_airtable2(title = '', sub1_status = None, sub1_time = None, sub2_status = None, sub2_time = None,sub3_status = None, sub3_time = None,sub4_status = None, sub4_time = None,sub5_status = None, sub5_time = None, AIRTABLE_TABLE_NAME = ''):
     endpoint = f'https://api.airtable.com/v0/{AIRTABLE_BASE_ID}/{AIRTABLE_TABLE_NAME}'
     headers = {
         "Authorization": "Bearer " + str(AIRTABLE_API_KEY),
@@ -187,6 +191,7 @@ def add_to_airtable2(sub1_status = None, sub1_time = None, sub2_status = None, s
           "records": [
             {
               "fields": {
+                "title": title,
                 "last_submission_status": sub1_status,
                 "last_submission_time": sub1_time,
                 "last_2th_submission_status": sub2_status,
@@ -254,6 +259,7 @@ def load_sample():
     create_table1('summary')
     create_table2('submission_history')
     create_table1('failed_problems')
+    create_table1('review_problems')
 
     # Opening JSON file
     f = open('sample.json')
@@ -270,47 +276,50 @@ def load_sample():
     for i in data:
       add_to_airtable1(i['title'], i['link'],i['difficulty'],i['status'],str(i['submission_history']),i['submission_times'], 'summary')
       
-      if(len(i['submission_history']) < 5):
 
-        sub_status = []
-        sub_time = []
-        num = len(i['submission_history'])
+      #create submission history
+      #if(len(i['submission_history']) < 5):
 
-        for j in range(num):
+      sub_status = []
+      sub_time = []
+      num = len(i['submission_history'])
+
+      
+      for j in range(num):
           #print(j)
+        if(j >= 5): break
+        else:
           sub_status.append(i['submission_history'][-(j+1)]['submission_status'])
           print(sub_status[j])
           sub_time.append(i['submission_history'][-(j+1)]['submission_time'])
-        
+
+      if num < 5:    
         for k in range(num , 5) :
           sub_status.append(None)
           sub_time.append(None)
 
-        add_to_airtable2(sub_status[0], sub_time[0], sub_status[1], sub_time[1], sub_status[2], sub_time[2], sub_status[3], sub_time[3], sub_status[4], sub_time[4], 'submission_history')
+      add_to_airtable2(i['title'],sub_status[0], sub_time[0], sub_status[1], sub_time[1], sub_status[2], sub_time[2], sub_status[3], sub_time[3], sub_status[4], sub_time[4], 'submission_history')
 
-      else: add_to_airtable2(i['submission_history'][-1]['submission_status'],\
-                          i['submission_history'][-1]['submission_time'],\
-                          i['submission_history'][-2]['submission_status'],\
-                          i['submission_history'][-2]['submission_time'],\
-                          i['submission_history'][-3]['submission_status'],\
-                          i['submission_history'][-3]['submission_time'],\
-                          i['submission_history'][-4]['submission_status'],\
-                          i['submission_history'][-4]['submission_time'],\
-                          i['submission_history'][-5]['submission_status'],\
-                          i['submission_history'][-5]['submission_time'],\
-                          'submission_history')
+  
+    #create review history
+    reviewlist = []
+    #for m in range(5):
+      #if(not sub_status[m].contains("Accepted")):
+        #reviewlist.append(i)
 
 
-    # Closing file
-    f.close()
+
+   
     
-    for i in data:
-      add_to_airtable1(i['title'], i['link'],i['difficulty'],i['status'],str(i['submission_history']),i['submission_times'], 'failed_problems')
+    #create failed problems
+  
+    add_to_airtable1(i['title'], i['link'],i['difficulty'],i['status'],str(i['submission_history']),i['submission_times'], 'failed_problems')
       
                  
 
 
     # Closing file
+    f.close()
     f2.close()
 
 #load sample
